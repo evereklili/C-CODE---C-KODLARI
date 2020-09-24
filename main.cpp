412de2e846baa624c119ea97f3e7e864c96c7215
OMER DOGU  www.omerdogu.com <17455568+evereklili@users.noreply.github.com>
2020-09-24 06:17:35 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,267 +1,272 @@
-/*
-#include<stdio.h>
-int main()
-{
-    int saniye, dakika, saat;
-    double sonuc;
-    scanf("%d %d %d",saat,dakika,saniye);
-    printf("Girilen saat değerleri %d saat, %d dakika, %d saniye olarak girdiniz. Simdi cevirme islemini yapiyorum. ",saat, dakika, saniye);
-    printf("Cevrilen deger: ", saat*3600+60*dakika+saniye);
-
-
-
-    return 0;
-}
-*/
-/*
- *
-#include<stdio.h>
-int main(){
-     double x,v;
-     scanf("%lf %lf",&x,&v);
-     printf("%lf",x/v);
+ /*
+  * #include<stdio.h>
+ int main()
+ {
+     if(1071&1023)
+     {
+         if(!0)
+             printf("Ömer\n");
+             else
+                 printf("Faruk \n");
+                 
+     }
      return 0;
-
-}
-*/
-/*
-#include<stdio.h>
-int main()
-{
-    int pi=3.14;
-    double r=0;
-    scanf("%lf,",&r);
-    printf("Alani: %lf ",4*pi*r*r);
-    printf("Hacmi: %lf ",4*pi*r*r*r/3);
+ }
+ */
+ /*
+ #include<stdio.h>
+ int main()
+ {
+     // girilen sayı tek mi çift mi sonucu ekran yazdır. 
+     int x;
+     printf("Sayinizi giriniz:");
+     scanf("%lf",&x);
+     if((x%2==0))
+     {
+         printf("Sayiniz çifttir.");
+         
+     }
+     else
+         printf("Sayiniz tektir.");
+         
+  return 0;
+ }
+ 
+ */
+ /*
+ #include<stdio.h>
+ int main()
+ {
+     double x,y;
+     scanf("%lf %lf",&x,&y);
+     if(x<y)
+     {
+         printf("1.sayiniz kucuktur. \n");
+         
+     }else if(y<x)
+     {
+         printf("2.sayiniz kucuktur.\n");
+     }
+     else
+         printf("Sayilariniz esittir. \n");
+ }
+ */
+ /*
+ #include<stdio.h>
+ int main(){
+     bas:
+     int x,y,z;
+     scanf("%d %d %d",&x,&y,&z);
+     if(x>1000||y>1000||z>1000||x<-1000||y<-1000||z<-1000){
+          printf("[-1000,1000] araliginda olmalidir."); goto bas;
+     }
+        else{
+         
+             if(x>y&&y>z)
+                 printf("%d",x);
+                 else if(y>x&&x>z)
+                 {
+                     printf("%d",y);
+                     
+                 }
+                 else if(z>x&&x>y)
+                     printf("%d",z);
+                     
+        }   
     return 0;
-}
-*/
-
-/*
-#include<stdio.h>
-int main()
-{
-    int saat, dakika;
-    scanf("%d,%d",&saat,&dakika);
-    printf("%d",saat*30-6*dakika);
-    return 0;
-
-
-
-
-}
-*/
-/*
-#include<stdio.h>
-int main()
-{
-
-    double a,b,c,d,e;
-    scanf(" %lf %lf %lf %lf %lf ", &a, &b, &c, &d, &e);
-    printf("%lf sayinin ortalamasi ",(a+b+c+d+e)/5);
-
-
-
-
-    return 0;
-
-
-
-}
-*/
-/*
-#include<stdio.h>
-int main()
-{
-    /* size NxN 'lik bir matris verilsin. Matrisin içi (0,0) başlamak üzere soldan sağa artan sayılar yazılsın. ve aynı şekilde alta devam edilsin.
-     * verilen sayılar hangi satır ve sutunda olduğunu gösteren isteniyor. Bize verilen ise, NxN deki N sayısı ve ikincisi ise sayının kaç olduğudur.
-     * programı yazınız.
-     * Her bir satır N adet sayıdan oluştuğundan verilen sayı N'ye bölünürse, kaç satır geçtiğini bulunur. N ye bölünürse kaç satır geçtiği bulunur.
-     * Ancak satır numarası için 1 eklenmesi gerekir. Örneğin 2 sayısı 2/5=0 çıkar Ancak satır numarasına 1 eklenmelidir
-     * Benzer şekilde, sayı N'ye tam bölünüyorsa en baştaki sütunda, N'ye bölümünden kalan 1 ise ikinci sütunda... Haliyle sütunu bulmak için sayının N'ye bölümünden kalana 1 eklemek gerekir.
-     * */
-/*
-int N, sayi;
-scanf("%d, %d ",&N,&sayi);
-printf("%d %d \n", sayi/N+1, sayi%N+1);
-
-
-return 0;
-}
-*/
-/*
- *
-
-#include<stdio.h>
-int main()
-{
-    int a,b,c,d;
-    scanf("%d %d %d %d",&a,&b,&c,&d);
-    printf("%d %d %d %d \n" ,a,d,b,c);
-    return 0;
-}
-*/
-/*
-#include<stdio.h>
-int main()
-{*/
-    /*
-     * bir doğum günü kutlamasına, N kişi gelmiştir. Kutlama hakkında ertesi gün 5 farklı köşe yazısı çıkmıştır.
-     * Yazarlar, yazıları içersinde gelen kişi saysını tahmini olarak vermişlerdir. Size metrekareye düşen insan sayısı veriliyor. Buna göre
-     * yazarların, yazılarında verilen kişi sayılarının gerçekliğe yaknlığını bulan programı yazınız. Verilen ilk sayı kutlamasının kaç metrekarede yapıldığını, ikinci sayı ise kaç metrekareye düşen insan sayısıdır. Sonraki 5 sayı ise yazarların belirttiği sayıyı göstermektedir.
-     * */
-     /*
-    int n,x,y;
-    int a,b,c,d,e;
-    scanf("%d %d",&x,&y);
-    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
-    n=x*y;
-    printf("%d %d %d %d %d",a-n,b-n, c-n,d-n,e-n);
-    return 0; 
-}
-
-*/
-/*
-
-#include<stdio.h>
-int main(){
-    int sayi;
-    int a,b,c,d;
-    scanf("%d",&sayi);
-    a=(sayi/1000)%10;
-    b=(sayi/100)%10;
-    c=(sayi/10)%10;
-    d=(sayi/1)%10;
-    printf("%d", a+b+c+d);
-    return 0;
-}
-*/
-/*
-#include<stdio.h>
-int main()
-{
+ } 
+ // çözüm oluşmamıştır. 
+ */
+ /*
+  * 
+ 
+ #include<stdio.h>
+ int main()
+ {
+     int a,b,c, maxx=-10000;
+     scanf("%d, %d, %d",&a,&b,&c);
+     if(a>maxx)
+         maxx=a;
+         if(b>maxx)
+             maxx=b;
+             if(c>maxx)
+                 maxx=c;
+                 printf("%d", maxx);
+                 return 0;
+                 
+ }
+ 
+ */
+ /*
+ #include<stdio.h>
+ int main(){
+    //a ve b tamsayilari, ekrana sirasi ile şu durumda şartı sağliyorsa ilk durumu yazdirmalidir. 
+    // girilen sayilar eşitse, eşittir, ikinci sayi birinci sayiyi tam bölüyorsa, b/a; bu durumun hiç birini sağlamaz ise belirsiz yazdırmalıdır. 
     int a,b;
-    printf("a ve be degerlerini giriniz:\n");
-    scanf("%d %d",&a,&b);
-    if(a>b)
-    {
-        printf("a>b");
-        
-    }
-    if(a<b)
-    {
-        printf("a<b");
-    }
+    scanf("%d, %d",&a,&b);
     if(a==b)
     {
-        printf("a=b");
+        printf(" sayilar esittir.");
         
     }
+    else if(a%b==0) 
+        {
+            printf("%d |%d",b,a);
+        }
+    else 
+        {
+            printf("Belirsizlik mevcuttur.");
+        }
     return 0;
-}
-*/
+    
+ }
+ */
+ 
 /*
+ * 
 #include<stdio.h>
 int main()
 {
-    bas:
-    int a;
-    printf("0 ile 100 arasinda deger giriniz:\n");
-    scanf("%d",&a);
-    if(a>=1000)
-        printf("a sayisi 4 basamaklidir \n");
-        else if(a>=100)
-            printf("a sayisi 3 basamaklidir\n");
-            else if(a>=10)
-                printf("a sayisi 2 basamaklidir.\n");
-                else if(a>1)
-                    printf("a bir basamaklidir \n");
-                    else 
-                        printf("a sayisi ya 9999 büyük ya da negatif sayidir.\n");
-                        goto bas;
+    int L,G;
+    scanf("%d, %d",&L,&G);
+    if(L<35) printf("kisa boylu");
+    else if(L<65) printf("orta boylu");
+    else printf("uzun boylu");
     
+    if(G<45) printf("zayif");
+    else if(G<70) printf("normal");
+    else printf("agir");
     return 0;
+    
+    
 }
 */
+/*
+// verilen üç adet programı küçükten büyüğe doğru sıralayan programı bulunuz.(tamsayiyi)
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d, %d, %d",&x,&y,&z);
+    if(x<=y && y<=z)  printf("%d %d %d ",x,y,z);
+    else if(x<=z&&z<=y) printf("%d %d %d ",x,y,z);
+    else if(y<=x&&x<=z) printf("%d %d %d ",y,x,z);
+    else if(y<=z&&z<=x) printf("%d %d %d ",y,z,x);
+    else if(z<=x&&x<=y) printf("%d %d %d ",z,x,y);
+    else if(z<=y&&y<=x) printf("%d %d %d ",z,y,x);
+    
+    return 0;
+    
+
+}
+*/
+/*
+ * 
+//sıfır 10 aralıiğinda olan sayıların okunuşunu yazan programı yazınız. 
+#include<stdio.h>
+int main()
+{
+    bas: 
+    int x;
+    scanf("%d",&x);
+    switch(x) 
+    {
+        case 1:printf("Bir"); break;
+        case 2:printf("İki"); break;
+        case 3:printf("Uc"); break;
+        case 4:printf("Dort"); break;
+        case 5:printf("Bes"); break;
+        case 6:printf("Alti"); break;
+        case 7:printf("Yedi"); break;
+        case 8:printf("Sekiz"); break;
+        case 9:printf("Dokuz"); break;
+        case 10:printf("On"); break;
+        default: printf("Durum yok. Sayiyi 0 ile 10 arasinda giriniz."); goto bas;
+    }
+    return 0;
+}
+
+*/
+/*
+#include<stdio.h>
+int main(){
+    int a,b,c,d,e;
+    scanf("%d, %d, %d, %d, %d",&a,&b,&c,&d,&e);
+    if(a%2==0&&b%2==0&&c%2==0&&d%2==0&&e%2==0) printf("Tumu cift sayidir.");
+    else if(a%2==1&&b%2==01&&c%2==1 && d%2==1 &&e%2==1) printf("Tumu tek sayidir.");
+    else printf("Karisik ortalik.");
+    return 0;
+    
+}*/
 
 /*
-#include<stdio.h>
-int main(){
-    int i=3, j=4, k=5,m=2;
-    printf("%d",!k);
-    printf("%d",!(k-m-i));
-    printf("%d",!(j>i));
-    printf("%d",k+j>=i+m&&j==0);
-    printf("%d",k<=m||k==m+i);
-    
-    return 0;
-}
-*/
-/*
-#include<stdio.h>
-int main(){
-     
-    int a,b,c;
-    printf("a,b,c deger giriniz:");
-    scanf("%d,%d,%d",&a,&b,&c);
-    if(a<b && b<c)
-        printf("a<c \n");
-    if(a>0 || b>0 || c>0)
-        printf("En az birisi sifirdan farklidir.");
-        
-     
-    return 0;
-}
-*/
-/*
+ * 
  * 
 #include<stdio.h>
-int main()
-{
-    int a,b,c;
-    printf("a ve b degerlerini giriniz: ");
-    scanf("%d %d", &a, &b);
-    (a>=0)?printf("negatif degil \n"):printf("negatif \n");
-    c=((b==0)?0:1);
-    printf("b nin dogruluk degeri %d dir.",c);
-    return 0;
-}
-*/
-/*
- * 
-#include<stdio.h>
-int main()
-{
-    int plaka;
-    printf("plaka kodunu giriniz:");
-    scanf("%d",&plaka);
-    switch(plaka){
-        case 1: printf("Adana"); break;
-        case 34: printf("İstanbul"); break;
-        case 26:printf("Eskişehir"); break;
-        case 44:printf("Malatya"); break;
-        
-    }
+int main(){
     
-    return 0;
-}
-*/
-/*
-#include<stdio.h>
-int main()
-{
-    printf("a degerini giriniz:");
-    int a;
-    scanf("%d",&a);
-    switch(a)
+    int ay;
+    scanf("%d",&ay);
+    if(ay<0||ay>12)
     {
-        case 3>2: printf("1"); break;
-        case 4: printf("4"); break;
-        default: printf("bitti.. \n");
-        case 2: printf("2"); break;
-        case 3: printf("3"); break;
+        printf("0");
+    }
+    else if(ay==4||ay==6||ay==9||ay==11)
+        printf("30");
+    else if(ay==2) //subat ayi içindir
+        printf("28");
+        else 
+            printf("31"); //tum iflerden geçerse 31 gunluk aylar kalır. 
+                
+    
+    return 0;
+    
+}
+*/
+/*
+#include<stdio.h>
+int main()
+{
+    int musteri_tipi;
+    double alisveris_tutari;
+    printf("Musteri Tipi: 1.Ogrenci, 2.Ogretmen, 3. Diger\n");    
+    scanf("%d",&musteri_tipi);
+    printf("AlisVeris Tutari Giriniz: \n");
+    scanf("%lf",&alisveris_tutari);
+    double a,b,c;
+    a=(alisveris_tutari-alisveris_tutari*10/100);
+    b=(alisveris_tutari-alisveris_tutari*20/100);
+    c=(alisveris_tutari-alisveris_tutari*1/100);
+    switch(musteri_tipi)
+    {
+        case 1: printf("%lf",a); break;
+        case 2: printf("%lf",b); break;
+        case 3: printf("%lf",c);break;
+        default: printf("Diger durumlar canim..."); break;
         
     }
+    
     return 0;
 }
 */
+#include<stdio.h>
+int main()
+{
+    int gun, ay, yil;
+    scanf("%d, %d, %d",&gun,&ay, &yil);
+    if(yil<0||yil>9999)
+    {
+        printf("0");
+        
+    }
+    else if(ay<1||ay>12)
+    {
+        printf("0");
+        
+    }else if((ay==4||ay=6||ay==9||ay==11)&&(gun<1||gun>30))
+    {
+        printf("0");
+        
+    }
+    
+}
